e531b6bc39a16e04589f943c108bd3cb916bf04c
Johnskyle Sebastian <bestiejohn10@gmail.com>
2023-09-09 13:56:14 +0800

    update profile zoom

--- src/components/Home.js
+++ src/components/Home.js
@@ -52,11 +52,38 @@ function Home() {
     }
   };
 
+  const zoomPic = () => {
+    const pic = document.querySelector('.profilePic')
+    const picContainer = document.querySelector('.pictureContainer')
+    
+    if (pic.classList.contains('.zoom')) {
+      pic.classList.remove('.zoom')
+      picContainer.removeEventListener('click', zoomPic)
+      pic.style.borderRadius = '50%'
+      pic.style.width = '60%'
+      picContainer.style.width = '100%'
+      picContainer.style.height = '220px'
+      picContainer.style.left = '0'
+      picContainer.style.top = '0'
+      picContainer.style.background = 'transparent'
+    } else {
+      picContainer.addEventListener('click', zoomPic)
+      pic.classList.add('.zoom')
+      pic.style.borderRadius = '0'
+      pic.style.width = '40%'
+      picContainer.style.width = '100vw'
+      picContainer.style.height = '100vh'
+      picContainer.style.left = '-16px'
+      picContainer.style.top = '-16px'
+      picContainer.style.background = 'black'
+    }
+  }
+
   return (
     <div className="homeRootContainer">
       <div className="profileContainer">
         <div className="pictureContainer">
-          <img src={myPic} alt="Profile Pic" className="profilePic" />
+          <img src={myPic} alt="Profile Pic" className="profilePic" onClick={zoomPic} />
         </div>
         <div className="profilePicDetails">
           <Typography variant="h6" className="name">
